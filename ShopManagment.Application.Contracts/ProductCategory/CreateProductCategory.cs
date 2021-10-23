e0bafe3a929ed5da8383290e5a5672cdb0a2b944
HusseinBeygi <husseinbeygi.dev@gmail.com>
2021-10-23 21:36:36 +0330

    Validation FIXED!

--- ShopManagment.Application.Contracts/ProductCategory/CreateProductCategory.cs
+++ ShopManagment.Application.Contracts/ProductCategory/CreateProductCategory.cs
@@ -1,16 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
 
 namespace ShopManagement.Application.Contracts.ProductCategory
 {
     public class CreateProductCategory
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Name { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Description { get; set; }
         public string Picture { get; set; }
         public string PictureAlt { get; set; }
         public string PictureTitle { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string KeyWords { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string MetaDescription { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Slug { get; set; }
 
     }
